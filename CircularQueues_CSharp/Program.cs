09f7c2a05f42a4898d1ef4ce849dd89b344cc970
Izalhadji31 <115075859+Izalhadji31@users.noreply.github.com>
2022-12-14 16:57:39 +0700

    public void insert

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -13,6 +13,14 @@ namespace CircularQueues_CSharp
         int[] queue_array = new int[5];
         public Queues()
         {
+            /*Intializing the values of the variables REAR amd FRONT to -1
+             * to indicate that the queue is intially empty. */
+            FRONT = -1;
+            REAR = -1;
+        }
+        public void insert(int element)
+        {
+
         }
     }
 }
