b0b3a3c603038fd5e2960d1d78d7fb97645f5bdc
Izalhadji31 <115075859+Izalhadji31@users.noreply.github.com>
2022-12-14 17:10:53 +0700

    menambahkan public void remove

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -20,6 +20,36 @@ namespace CircularQueues_CSharp
         }
         public void insert(int element)
         {
+            /* This statement checks for the overflow condition. */
+            if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+            {
+                Console.WriteLine("\nQueue overflow\n");
+                return;
+            }
+            /* The folloeing statement cheks whether the queue is empty. If the queue is
+             * empty, then the value of the REAR and FRONT variables is set to 0.*/
+            if (FRONT == -1)
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
+            else
+            {
+                /* If REAR is at the last position of the array, then the value of
+                 * REAR is set to 0 that corresponds to the first position of the array. */
+                if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    /* If REAR is not at the last poition, then its value is
+                     * incremented by one. */
+                    REAR = REAR + 1;
+            }
+            /* Once the position of REAR is determined, the element is added at
+             * its proper place. */
+            queue_array[REAR] = element;
+        }
+        public void remove()
+        {
 
         }
     }
